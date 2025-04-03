f68beab50d2a8d75e6c1c45c6efea2bd87935307
limdaecheol <eocjf852963@gmail.com>
2025-04-03 19:02:46 +0900

    feat: 댓글 조회 API 기능 추가

--- Django_prac/likelion13/posts/urls.py
+++ Django_prac/likelion13/posts/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from posts.views import *
 
 urlpatterns = [
-    path('', hello_world, name = 'hello_world'),
-    path('<int:id>', get_post_detail), # Ãß°¡
+
+		#path("", hello_world, name="hello_world"),
+    #path("page", index, name="my-page"),
+    #path('<int:id>', get_post_detail),
+
+    path('', post_list, name="post_list"),
+    path('<int:post_id>/', post_detail, name='post_detail'), 
+    path('posts/<int:post_id>/comments/', get_comment_all, name='comment_list_by_post'), # ???? ?? ?? ?? ????
 ]
