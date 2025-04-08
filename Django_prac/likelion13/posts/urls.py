85cd8c071f6c6caa44fdf258d4c8aeb45ee3a49e
limdaecheol <eocjf852963@gmail.com>
2025-04-08 14:10:01 +0900

    feat: 인코딩 깨진 주석 수정

--- Django_prac/likelion13/posts/urls.py
+++ Django_prac/likelion13/posts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
 
     path('', post_list, name="post_list"),
     path('<int:post_id>/', post_detail, name='post_detail'), 
-    path('posts/<int:post_id>/comments/', get_comment_all, name='comment_list_by_post'), # ???? ?? ?? ?? ????
-    path('categories/<int:category_id>/posts/', posts_by_category, name='posts_by_category'), # ????? ??? ?? 
+    path('posts/<int:post_id>/comments/', get_comment_all, name='comment_list_by_post'), # 게시글에 달린 댓글 조회
+    path('categories/<int:category_id>/posts/', posts_by_category, name='posts_by_category'), # 카테고리에 해당하는 게시글 조회
 ]
